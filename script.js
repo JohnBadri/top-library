5b3d9091dc5a9a8f2fc51c121374d84b1e0ab8fb
JohnBadri <135007003+JohnBadri@users.noreply.github.com>
2023-10-22 18:17:15 +1100

    Created functionality which adds book to the list of tables

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const myLibrary = [];
 const addBookButton = document.querySelector(".add-book");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector("form span");
+const form = document.querySelector("form");
+const tbody = document.querySelector("tbody");
 
 addBookButton.addEventListener("click", () => {
   modal.showModal();
@@ -11,10 +13,43 @@ closeModal.addEventListener("click", () => {
   modal.close();
 });
 
-function Book() {
-  // the constructor...
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+modal.addEventListener("submit", function (event) {
+  const bookName = document.querySelector(".book").value;
+  const authorName = document.querySelector(".author").value;
+  const numberPages = document.querySelector(".pages").value;
+  const haveYouRead = document.getElementById("read").value;
+  addBookToLibrary(bookName, authorName, numberPages, haveYouRead);
+  modal.close();
+});
+
+function Book(name, author, page, read) {
+  this.name = name;
+  this.author = author;
+  this.page = page;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(name, author, page, read) {
+  const newBook = new Book(name, author, page, read);
+  myLibrary.push(newBook);
+
+  const addTr = document.createElement("tr");
+  tbody.appendChild(addTr);
+
+  Object.entries(newBook).forEach(([key, value]) => {
+    const addInnerTd = document.createElement("td");
+    addInnerTd.textContent = value;
+    addTr.appendChild(addInnerTd);
+  });
+
+  const editStatus = document.createElement("td");
+  editStatus.innerHTML = `<button class="status">Change Status</button>`;
+  const deleteBook = document.createElement("td");
+  deleteBook.innerHTML = `<button class="delete">Delete Book</button>`;
+
+  addTr.append(editStatus, deleteBook);
 }
